3030fe37119a6fac74091c770780ab99bfaf2ad6
HSPMURTHY <parashivamurthy.hs@gmail.com>
2025-03-19 14:01:36 +0530

    Hello World

---
+++ myClass.java
@@ -0,0 +1,16 @@
+package HelloWorld;
+
+public class myClass {
+
+
+	   /* This is my first java program.
+	    * This will print 'Hello World' as the output
+	    */
+	   //System.out.println("Hello World");
+
+	   public static void main(String []args) {
+	      System.out.println("Hello World"); // prints Hello World
+	      System.out.println("Hello World"); // prints Hello World
+	      System.out.println("Coying again for test Hello World"); // prints Hello World
+	   }
+	}
